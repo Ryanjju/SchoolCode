cf31e50231d99a146823299ece69cd3ad16410c3
Ryan <113769772+Ryanjju@users.noreply.github.com>
2025-10-03 11:28:56 +0200

    Add Taxi class with number handling

---
+++ Taxi/Taxi.java
@@ -0,0 +1,11 @@
+public class Taxi {
+    private int taxi_nr; 
+
+    public void Taxi(int number) {
+        taxi_nr = number;
+    }
+
+    public int giveNumber() {
+        return taxi_nr;
+    }
+}
