52d0a12568e61aea1ee55fc95b27e772252a8eb0
Ryan <113769772+Ryanjju@users.noreply.github.com>
2025-11-13 08:17:15 +0100

    Add Main class to compare two strings

---
+++ CompareTo/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        String test1 = "Herbert";
+        String test2 = "Hilde";
+        
+
+        int cmp = test2.compareTo(test1);
+
+
+
+        System.out.println("Comapre: " + cmp);
+    }
+}
